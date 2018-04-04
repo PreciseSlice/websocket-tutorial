39e3f7ed974bf38224337b22d98bbd1632735903
Spencer Herms <slherms@gmail.com>
2018-04-03 21:32:20 -0600

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+io.on('connection', socket => {
+  socket.broadcast.emit(
+    'userConnected',
+    `A new user, ${Date.now()}, has connected.`
+  );
+
+  socket.on('chat message', msg => {
     io.emit('chat message', msg);
+  });
+
+  socket.on('typing', msg => {
+    socket.broadcast.emit('typing', msg);
+  });
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('userDisconnected', 'A user has disconnected');
   });
 });
 
-http.listen(port, function(){
+http.listen(port, () => {
   console.log('listening on *:' + port);
 });
